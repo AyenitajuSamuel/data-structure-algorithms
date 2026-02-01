c21be8cb52228877ff0a642ca14d2262af34496a
Samuel <samuelayenitaju@gmail.com>
2026-02-01 14:14:15 +0100

    Solved concatenation_of_array

---
+++ Arrays/concatenation_of_array.py
@@ -0,0 +1,31 @@
+"""
+Problem Description:
+    Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+    Specifically, ans is the concatenation of two nums arrays.
+
+    Return the array ans.
+
+Approach: 
+    To solve this problem, we can simply concatenate the input array with itself. This can be done using list concatenation in Python.
+
+    Steps:
+    1. Take the input array `nums`.
+    2. Create a new array `ans` by concatenating `nums` with itself.
+
+Constraints:
+    n == nums.length
+    1 <= n <= 1000
+    1 <= nums[i] <= 1000
+"""
+
+class Solution:
+    def getConcatenation(self, nums):
+        ans = nums * 2
+        return ans
+
+solution = Solution()
+
+#test
+num_list = [1,3,2,1]
+print(solution.getConcatenation(num_list))
